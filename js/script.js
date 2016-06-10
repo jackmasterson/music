05c583b3051e8ec2910cc6482a60c97f4a3f80ad
JackMasterson <jackmasterson5@gmail.com>
2016-06-10 17:16:11 -0400

    Add background to splash page, along with "Search/Save/Share" div pics

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//key = RCSWLKK118ZODUFE
+
+var model = {
+	headImg: [
+		{
+			src:'img/search.png',
+			text: "Search"
+		}, 
+		{
+			src:'img/save.png',
+			text: "Save"
+		}, 
+		{
+			src:'img/friends.png',
+			text: "Share"
+		}
+	]
+};
+
+var viewModel = {
+
+	init: function(){
+
+	}
+};
+
+ko.applyBindings(viewModel.init());
