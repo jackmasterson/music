447d90154e8ad0ea276ec0a2e43207e124cf43fa
JackMasterson <jackmasterson5@gmail.com>
2016-06-10 17:42:20 -0400

    Add AJAX request - search not working properly

--- js/script.js
+++ js/script.js
@@ -20,7 +20,46 @@ var model = {
 var viewModel = {
 
 	init: function(){
+		musicView.init();
+	}
+};
 
+var musicView = {
+
+	init: function() {
+		var that = this;
+		var searchBar = document.getElementsByClassName('search')[0];
+
+
+		$('.search-button').click(function(){
+			val = $(searchBar).val();
+			//console.log(val);
+			that.key = "RCSWLKK118ZODUFE";
+			that.url = "https://freemusicarchive.org/api/trackSearch?q="+val+"&limit=10"
+			 + that.key;
+			that.render();
+
+		});
+
+			
+		
+			
+
+	
+
+		
+	},
+
+	render: function(){
+
+		var that = this;
+		$.ajax({
+			url: this.url,
+			dataType: 'json'
+		})
+		.done(function(response){
+			console.log(response);
+		});
 	}
 };
 
