ab5b3dba6e628c74797b9b14164a981f71c4a3c2
JackMasterson <jackmasterson5@gmail.com>
2016-06-14 22:29:50 -0400

    Clean things up a bit

--- public/js/script.js
+++ public/js/script.js
@@ -155,6 +155,10 @@ var toggle = {
       model.albumInfo.removeAll();
       model.trackInfo.removeAll();
 
+      //instead of removing all, why not just add the latest?
+      //that way the add/subtract counter won't get messed up
+      //every time you add a new one
+
       var modelTitle, clickedTitle, matchedArt, matchedAlb, matchedTrack,
         modelType, typeId;
       var title;
@@ -169,21 +173,27 @@ var toggle = {
 
 
         var type = fav.type;
+        var artFav = fav.artist;
+        var artClick = clicked.artist;
         var artMatch = type === 'Artist';
+        var noArtDup = artFav !== artClick;
         var albMatch = type === 'Album';
         var trackMatch = type === 'Track';
         $('icons.each').hide();
 
         if(artMatch){
-          console.log(fav);
+        //  console.log(fav);
 
           var el = document.getElementById(fav.type);
           el.style.color = 'red';
+
           model.artistInfo.push(fav);
           typeId = "#"+fav.type;
           $("artistSearch").show();
           $(typeId).trigger('click');
 
+          console.log(model.artistInfo());
+          console.log(fav);
           //should include a color-change here
 
         }
